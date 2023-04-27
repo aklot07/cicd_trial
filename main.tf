c680113d9d7710d3fbd77d646d965ffbca1b2236
aklot <anoopkiran1996@gmail.com>
2023-04-27 22:54:28 +0530

    ak 1

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "akrsg" {
+  name = "AK-k8s"
+}
+
+resource "azurerm_kubernetes_cluster" "ak_k8s" {
+  name                = "ak-k8s"
+  location            = "East Asia"
+  resource_group_name = data.azurerm_resource_group.akrsg.name
+  dns_prefix          = "akdnsprefix"
+  kubernetes_version  = "1.25.6"
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+    zones      = ["1"]
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
